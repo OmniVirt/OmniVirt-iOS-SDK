a2678f4cb06599c80050ad97d0fcbfa034f49e04
Jatupon Sukkasem <jasukkas@Jatupons-MacBook-Pro.local>
2017-05-24 13:41:39 -0700

    Updating prepare_command for podspec file.

--- install_run_script.rb
+++ install_run_script.rb
@@ -3,7 +3,7 @@
 require 'pathname'
 require 'xcodeproj'
 
-path_to_xcode_build_script = '"${SRCROOT}/strip-frameworks.sh"'
+path_to_xcode_build_script = "#{File.dirname(__FILE__)}/strip-frameworks.sh"
 xcode_build_script_name = 'Strip VRKit Framework'
 
 puts Dir.pwd
@@ -17,24 +17,14 @@ if path_to_spec.start_with?('/private/tmp/CocoaPods/Lint')
 
   puts 'CocoaPods linting, bail now before fail'
   exit 0
-elsif path_to_spec.include?('.cocoapods')
-  # Pod installed from spec repo
-  # podspec: /Users/ben/.cocoapods/repos/REPO_NAME/POD_NAME/1.0.0/POD_NAME.podspec
-  # Dir.pwd: /Users/ben/APP_PATH/Pods/POD_NAME
-
-  path_to_project = Dir.glob(Pathname.new(Dir.pwd) + '../../*.xcodeproj')[0]
 else
-  # Pod installed via :path in Podfile
-  # podspec: /Users/ben/LOCAL_POD_PATH/POD_NAME/POD_NAME.podspec
-  # Dir.pwd: /Users/ben/LOCAL_POD_PATH/POD_NAME
-
-  path_to_project = Dir.glob(Pathname.new(path_to_spec) + '../../../*.xcodeproj')[0]
+  path_to_project = Dir.glob("#{Dir.getwd}/*.xcodeproj")[0]
 end
 
 puts path_to_project
 
 if path_to_project.nil?
-	puts "Couldn't find Xcode project."
+	raise "Please change the current directory to root of the Xcode project and then run `pod install` to install OmniVirtSDK."
 else
 	project = Xcodeproj::Project.open(path_to_project)
 	main_target = project.targets.first
@@ -47,7 +37,7 @@ else
 	if (!script_installed)
 		puts "Installing run script in Xcode project #{path_to_project}"
 		phase = main_target.new_shell_script_build_phase(xcode_build_script_name)
-		phase.shell_script = path_to_xcode_build_script
+		phase.shell_script = File.open(path_to_xcode_build_script).read
 		project.save()
 	else
 		puts "Run script already installed"
